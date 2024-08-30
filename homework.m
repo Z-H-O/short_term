c5d5fee779df2b379e30d629c0f32eb762f48945
ZhouHongYi <2746486175@qq.com>
2024-08-30 20:51:12 +0800

    优化退出，增加个人信息

--- homework.m
+++ homework.m
@@ -31,9 +31,11 @@ function main()
    
     hFig = uifigure;
     hText1 = uicontrol('Style', 'text', 'Parent', hFig, 'Position', [75 300 200 50], 'String', '大数据23周弘毅');
-    hText2 = uicontrol('Style', 'text', 'Parent', hFig, 'Position', [75 275 200 50], 'String', '学号：23152090125');
+    hText2 = uicontrol('Style', 'text', 'Parent', hFig, 'Position', [75 275 200 50], 'String', '序号:163');
+    hText3 = uicontrol('Style', 'text', 'Parent', hFig, 'Position', [75 250 200 50], 'String', '学号:23152090125');
     set(hText1, 'FontSize', 14, 'FontWeight', 'bold', 'ForegroundColor', 'blue');
     set(hText2, 'FontSize', 14, 'FontWeight', 'bold', 'ForegroundColor', 'blue');
+    set(hText3, 'FontSize', 14, 'FontWeight', 'bold', 'ForegroundColor', 'blue');
     hButton = uibutton(hFig, 'push');
     set(hButton, 'Position', [275 200 100 50], 'Text', '继续', 'FontSize', 14, 'FontWeight', 'bold');
     set(hButton, 'ButtonPushedFcn', @(src, ~)buttonClicked(hFig));  % 设置按钮的回调函数
@@ -43,16 +45,20 @@ function main()
         close(hFig)  % 使用 hFig 句柄关闭对应的图形窗口
     end
     uiwait(hFig); % 等待当前窗口关闭
-   
+   %%播开始音乐
+    [start_y,start_fs]=audioread("champion.mp3");
+    sound(start_y,start_fs);
+    pause(8); % 播放音乐八秒
+    %%选择文件
         [wav_y,wav_Fs] = audioread("海浪.wav");%读取wav文件
         [mp3_y,mp3_Fs]=audioread("18 The Slopes of the Blessure.mp3");%读取MP3
-        disp("选择你要转化的文件")
+        disp("选择你要转换的文件")
         disp("1.海浪.wav")
         disp("2.18 The Slopes of the Blessure.mp3")
         a=input("请输入数字：");
         switch a
             case 1
-                disp("选择你要转化的文件")
+                disp("选择你要转换的文件")
                 disp("1.wav转成mp3")
                 disp("2.wav转成ogg")
                 b=input("请输入数字：");
@@ -95,21 +101,23 @@ function main()
                         music_play(y,fs)
                 end
         end
-        % hFig2=uifigure;
+    %     hFig2=uifigure;
         
-        % hButton2 = uibutton(hFig2, 'push');
-        % set(hButton2, 'Position', [275 200 100 50], 'Text', '退出', 'FontSize', 14, 'FontWeight', 'bold');   
+    %     hButton2 = uibutton(hFig2, 'push');
+    %     set(hButton2, 'Position', [275 200 100 50], 'Text', '退出', 'FontSize', 14, 'FontWeight', 'bold');   
     %     set(hButton2, 'ButtonPushedFcn', @(src, ~)exitbuttonClick2());
     %  %设置按钮的回调函数
     %     function exitbuttonClick2()
     %         exit;
     %     end
+    while true
         disp("请按Esc退出");
         waitforbuttonpress;
         key=get(gcf,'CurrentCharacter');
         if  uint8(key)==27
            exit;
         end
+    end
     
     
 end
